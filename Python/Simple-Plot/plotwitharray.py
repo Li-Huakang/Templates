45b0a01c88288a547c31de55edb94aea271d715b
kayasu <huakang.li@outlook.com>
2022-04-02 15:23:18 +0800

    fix simple plot script

--- Python/Simple-Plot/plotwitharray.py
+++ Python/Simple-Plot/plotwitharray.py
@@ -6,21 +6,20 @@ plt.style.use(['science','ieee'])
 x = [1, 2, 3, 4, 5]
 y = [1, 2, 3, 4, 5]
 
-with plt.style.context(['science','ieee']):
-    fig, ax = plt.subplots()
-    ax.plot(x,y,label="labelsomthing")
-    
-    ax.set(xlabel='this is x label')
-    ax.set(ylabel='this is y label')
-    ax.legend()
-    ax.autoscale(tight=True)
-    
-    ## save config
-    savename = "test"+".pdf"
-    if not os.path.exists("./figures"):
-        os.makedirs("figures")
-    fig.savefig("figures/"+savename)
-    cmd = "SumatraPDF " + "figures/"+savename
-    os.system(cmd)
+fig, ax = plt.subplots()
+ax.plot(x,y,label="labelsomthing")
+
+ax.set(xlabel='this is x label')
+ax.set(ylabel='this is y label')
+ax.legend()
+ax.autoscale(tight=True)
+
+## save config
+savename = "test"+".pdf"
+if not os.path.exists("./figures"):
+    os.makedirs("figures")
+fig.savefig("figures/"+savename)
+cmd = "SumatraPDF " + "figures/"+savename
+os.system(cmd)
 
 
